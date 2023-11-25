b6b30a99023208046e80061e463f6017f1b92b62
Shantanu Kulkarni <audentior153@gmail.com>
2023-11-25 15:38:51 +0530

    GLAD library added

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -6,6 +6,7 @@
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Events/MouseEvent.h"
 #include "Hazel/Events/KeyEvent.h"
+#include <glad/glad.h>
 
 //#include "Hazel/Renderer/Renderer.h"
 
@@ -72,6 +73,8 @@ namespace Hazel {
 		*/
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		HZ_CORE_ASSERT(status, "Failed to initialize glad!!!");
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -5,6 +5,7 @@
 
 #include <GLFW/glfw3.h>
 
+
 namespace Hazel {
 
 	class WindowsWindow : public Window
