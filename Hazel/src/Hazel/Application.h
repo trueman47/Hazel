32354926a33aa7f141a057190b131ee2a5c5625c
Shantanu Kulkarni <audentior153@gmail.com>
2024-11-03 20:07:24 +0530

    Adding shaders classes

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -7,6 +7,7 @@
 #include "Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
+#include "Hazel/Renderer/Shader.h"
 
 namespace Hazel {
 
@@ -32,6 +33,7 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 		static Application* s_Instance;
 	};
 
