449e3777a7aa51857a2a166cd76c4e0209fd465e
Shantanu Kulkarni <audentior153@gmail.com>
2024-11-13 18:30:11 +0530

    Abstracted out the renderercommand and api

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,8 +4,7 @@
 #include "Application.h"
 #include "Hazel/Log.h"
 #include "Input.h"
-
-#include <glad/glad.h>
+#include "Hazel/Renderer/Renderer.h"
 
 #include<glm/glm.hpp>
 
@@ -159,17 +158,26 @@ namespace Hazel {
 		//}
 
 		while (m_Running) {
-			glClearColor(0.1f, 0.1f, 0.1f, 1);
-			glClear(GL_COLOR_BUFFER_BIT);
+			/*glClearColor(0.1f, 0.1f, 0.1f, 1);
+			glClear(GL_COLOR_BUFFER_BIT);*/
+
+			RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+			RenderCommand::Clear();
+
+			Renderer::BeginScene();
 
 			m_BlueShader->Bind();
-			m_SquareVA->Bind();
+			/*m_SquareVA->Bind();
 			glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
-
+*/
+			Renderer::Submit(m_SquareVA);
 			m_Shader->Bind();
-			m_VertexArray->Bind();
+		/*	m_VertexArray->Bind();
 			glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+*/
+			Renderer::Submit(m_VertexArray);
 
+			Renderer::EndScene();
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
 
