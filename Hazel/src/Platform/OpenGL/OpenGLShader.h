b494df2923325f23284d5f5fe8c35c0015a248e1
Shantanu Kulkarni <audentior153@gmail.com>
2024-12-28 16:25:55 +0530

    Using shader libraries

--- Hazel/src/Platform/OpenGL/OpenGLShader.h
+++ Hazel/src/Platform/OpenGL/OpenGLShader.h
@@ -10,11 +10,12 @@ namespace Hazel {
 	{
 	public:
 		OpenGLShader(const std::string& filepath);
-		OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
+		OpenGLShader(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
 		virtual ~OpenGLShader();
 
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
+		virtual const std::string& GetName() const override { return m_Name; }
 		void UploadUniformInt(const std::string& name, int value);
 
 		void UploadUniformFloat(const std::string& name, float value);
@@ -30,6 +31,7 @@ namespace Hazel {
 		void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
 	private:
 		uint32_t m_RendererID;
+		std::string m_Name;
 	};
 
 }
