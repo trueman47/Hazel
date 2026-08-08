6d0a9fc47d44bf8be2f32f83d9f831494040a2fa
Shantanu Kulkarni <audentior153@gmail.com>
2026-08-08 20:44:04 +0530

    Added instrumentor

--- Sandbox/src/SandBoxApp.cpp
+++ Sandbox/src/SandBoxApp.cpp
@@ -203,8 +203,8 @@ class Sandbox : public Hazel::Application
 public:
 	Sandbox()
 	{
-		PushLayer(new ExampleLayer());
-		//PushLayer(new Sandbox2D());
+		//PushLayer(new ExampleLayer());
+		PushLayer(new Sandbox2D());
 	}
 
 	~Sandbox()
